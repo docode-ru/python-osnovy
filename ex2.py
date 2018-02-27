1103050dc4cb94c888029aa67372761047585481
Alexander Razzhivin <admin@httpbots.com>
2018-02-27 15:53:14 +0300

    ex2

---
+++ ex2.py
@@ -0,0 +1,24 @@
+# -*- coding=utf-8 -*-
+
+__ = False
+
+# Задание
+# Замените __ на соответствующие значения так,
+# чтобы не возникала ошибка
+assert __ == 20
+
+a = 5
+assert __ == a
+
+b = "hello"
+assert __ == b
+
+pi = 3.14
+assert __ == pi
+
+c = __
+assert c > 25
+
+assert __ == int
+assert __ == str
+assert __ == float
