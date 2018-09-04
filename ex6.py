8a6cd653a664ca762be070501002f88cb3cceb2a
Alexander Razzhivin <admin@docode.pro>
2018-09-04 13:34:27 +0300

    ex6

---
+++ ex6.py
@@ -0,0 +1,25 @@
+# -*- coding=utf-8 -*-
+
+__ = False
+
+# Задание
+# Замените __ на соответствующие значения и
+# исправьте все ошибки
+
+assert __ == True and True
+assert __ == False and False
+assert __ == True and 1
+assert __ == __ and False
+assert __ == 5 and 5
+assert __ == False or True
+assert __ == True or True
+assert (not __) == False or False
+assert __ == (not 0)
+assert __ == (not "")
+
+assert __ == ((9 > 7) and (2 < 4))
+assert __ == ((8 == 8) or (6 != 6))
+assert __ == (not (3 <= 1))
+
+assert __ == (True or (not True and not False))
+assert __ == (not((-0.2 > 1.4) and ((0.8 < 3.1) or (0.1 == 0.1))))
